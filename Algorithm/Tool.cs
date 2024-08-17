b4c9e76017c8efad0d8be76565a357ac6541d009
annie_su <LuAnn>
2024-08-17 16:24:21 +0800

    feat: 新增 Reverse 及 Merge

--- Algorithm/Tool.cs
+++ Algorithm/Tool.cs
@@ -76,5 +76,48 @@
 
             Swap(array, edge, r);
         }
+
+        /// <summary>
+        /// 反轉的範圍是 [l, r)
+        /// </summary>
+        /// <param name="array">反轉的 array </param>
+        /// <param name="l">包含</param>
+        /// <param name="r">不包含</param>
+        public static void Reverse(int[] array, int l, int r)
+        {
+            int count = (r - l) / 2 - 1;
+            for (int i = 0; i <= count; i++)
+            {
+                Swap(array, l + i, r - 1 - i);
+            }
+        }
+
+        /// <summary>
+        /// 將兩個以排序 array合併並重新排序，範圍是 [l, m) 及 [m, r)
+        /// </summary>
+        /// <param name="array">合併的array</param>
+        /// <param name="l"></param>
+        /// <param name="m"></param>
+        /// <param name="r"></param>
+        public static void Merge(int[] array, int l, int m, int r)
+        {
+            int[] copy = new int[array.Length];
+            array.CopyTo(copy, 0);
+            Reverse(copy, m, r);
+            int index = l;
+            r--;
+            while (l <= r)
+            {
+                if (copy[l] < copy[r])
+                {
+                    array[index] = copy[l++];
+                }
+                else
+                {
+                    array[index] = copy[r--];
+                }
+                index++;
+            }
+        }
     }
 }
